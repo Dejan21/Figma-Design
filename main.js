099c325f9d0686f0e57177b8655a74d0b1d3a987
Dejan21 <jangelovski_dejan@yahoo.com>
2023-02-23 12:14:00 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+function scrollDown() {
+    window.scroll({
+      top: 14110,
+    //   document.body.scrollHeight,
+      behavior: 'smooth'
+    });
+  }
+
+  
+function scrollToNextPart() {
+    window.scroll({
+      top: 9810,
+    //   document.body.scrollHeight,
+      behavior: 'smooth'
+    });
+  }
